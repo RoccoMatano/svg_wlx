3eb0bbc8550acfc1bbc3fe72684cc7f8f551a11b
Rocco Matano <rocco@matano.de>
2026-02-22 16:07:52 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+![svg_wlx](src/svg.svg "SVG")
+# svg_wlx
+
+[![License - MIT](https://img.shields.io/badge/license-MIT-green)](https://spdx.org/licenses/MIT.html)
+
+-----
+
+svg_wlx is a [Total Commander](https://www.ghisler.com/index.htm) lister
+plugin for viewing SVG files. It is based on
+[Nano SVG](https://github.com/memononen/nanosvg) (see [Nano SVG license
+](https://github.com/memononen/nanosvg/blob/master/LICENSE.txt)).
+I wrote it because I wanted something similar to
+[svgthumb](https://github.com/FrankEBailey/svgthumb), just a little different.
---
+++ src/svg_wlx.cpp
@@ -0,0 +1,404 @@
+
+#define WIN32_LEAN_AND_MEAN
+#define NANOSVG_ALL_COLOR_KEYWORDS
+#define NANOSVG_IMPLEMENTATION
+#define NANOSVGRAST_IMPLEMENTATION
+
+#include <windows.h>
+#include "nanosvg.h"
+#include "nanosvgrast.h"
+#include "listplug.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" int _fltused = 0;
+static ATOM wnd_class = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" BYTE __ImageBase;
+
+inline HINSTANCE hinst()
+{
+    return reinterpret_cast<HINSTANCE>(&__ImageBase);
+}
+
+BOOL WINAPI entry_point(HINSTANCE, DWORD reason, LPVOID)
+{
+    if (reason == DLL_PROCESS_DETACH && wnd_class)
+    {
+        UnregisterClass(MAKEINTRESOURCE(wnd_class), hinst());
+    }
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static NSVGimage* parse_file(PWSTR fname)
+{
+    NSVGimage* result = nullptr;
+    auto hdl = CreateFile(
+        fname,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        nullptr,
+        OPEN_EXISTING,
+        0,
+        nullptr
+        );
+    if (hdl != INVALID_HANDLE_VALUE)
+    {
+        auto size = GetFileSize(hdl, nullptr);
+        if (size != 0 && size != INVALID_FILE_SIZE)
+        {
+            auto* buf = static_cast<char*>(malloc(size + 1));
+            if (buf)
+            {
+                if (ReadFile(hdl, buf, size, nullptr, nullptr))
+                {
+                    buf[size] = 0;
+                    result = nsvgParse(buf, "px", 96.0f);
+                }
+                free(buf);
+            }
+        }
+        CloseHandle(hdl);
+    }
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static HBITMAP bitmap_from_svg(
+    PWSTR fname,
+    LONG max_width,
+    LONG max_height,
+    LONG& width,
+    LONG& height
+    )
+{
+    static NSVGrasterizer* rast = nullptr;
+    if (!rast)
+    {
+        rast = nsvgCreateRasterizer();
+        if (!rast)
+        {
+            return nullptr;
+        }
+    }
+
+    auto* svg = parse_file(fname);
+    if (!svg)
+    {
+        return nullptr;
+    }
+    if (svg->width < 1.0f || svg->height < 1.0f)
+    {
+        nsvgDelete(svg);
+        return nullptr;
+    }
+
+    auto scaleX = float(max_width) / svg->width;
+    auto scaleY = float(max_height) / svg->height;
+    auto scale = (scaleX < scaleY) ? scaleX : scaleY;
+    if (scale > 2.0f) scale = 2.0f;
+    auto w = int(svg->width * scale);
+    auto h = int(svg->height * scale);
+    if (w < 1) w = 1;
+    if (h < 1) h = 1;
+
+    auto* rgba = static_cast<BYTE*>(malloc(w * h * 4));
+    if (!rgba)
+    {
+        nsvgDelete(svg);
+        return nullptr;
+    }
+    memset(rgba, 0xff, w * h * 4);
+
+    nsvgRasterize(rast, svg, 0, 0, scale, rgba, w, h, w * 4);
+    nsvgDelete(svg);
+    width = w;
+    height = h;
+
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(bmi));
+    bmi.bmiHeader.biSize        = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth       = w;
+    bmi.bmiHeader.biHeight      = -h;   // top-down
+    bmi.bmiHeader.biPlanes      = 1;
+    bmi.bmiHeader.biBitCount    = 24;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    auto hdc = GetDC(nullptr);
+    BYTE* bgra = nullptr;
+    auto bmp = CreateDIBSection(
+        hdc,
+        &bmi,
+        DIB_RGB_COLORS,
+        reinterpret_cast<void**>(&bgra),
+        nullptr,
+        0);
+    ReleaseDC(nullptr, hdc);
+
+    if (bmp && bgra)
+    {
+        for (auto y = 0; y < h; y++)
+        {
+            for (auto x = 0; x < w; x++)
+            {
+                auto sidx = (y * w + x) * 4;
+                auto sr = rgba[sidx + 0];
+                auto sg = rgba[sidx + 1];
+                auto sb = rgba[sidx + 2];
+                auto sa = rgba[sidx + 3];
+
+                auto dr = (sr * sa + 255 * (255 - sa)) / 255;
+                auto dg = (sg * sa + 255 * (255 - sa)) / 255;
+                auto db = (sb * sa + 255 * (255 - sa)) / 255;
+
+                // rows are DWORD aligned
+                auto row_bytes = ((w * 3 + 3) & ~3);
+                int didx = y * row_bytes + x * 3;
+                bgra[didx + 0] = db;  // B
+                bgra[didx + 1] = dg;  // G
+                bgra[didx + 2] = dr;  // R
+                bgra[didx + 3] = sa;  // A
+            }
+        }
+    }
+    free(rgba);
+    return bmp;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct SvgCtxt
+{
+    HBITMAP bitmap;
+    LONG    width;
+    LONG    height;
+};
+
+inline SvgCtxt* get_ctxt(HWND hwnd)
+{
+    return reinterpret_cast<SvgCtxt*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+}
+
+static LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    RECT rc;
+
+    auto* ctxt = get_ctxt(hwnd);
+
+    switch (msg)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        GetClientRect(hwnd, &rc);
+        FillRect(hdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH));
+        if (ctxt && ctxt->bitmap)
+        {
+            HDC memDC = CreateCompatibleDC(hdc);
+            HGDIOBJ oldBmp = SelectObject(memDC, ctxt->bitmap);
+
+            auto wwidth = rc.right - rc.left;
+            auto wheight = rc.bottom - rc.top;
+            LONG dx, dy, stretched_w, stretched_h;
+            if (wwidth >= ctxt->width && wheight >= ctxt->height)
+            {
+                // center image
+                dx = (rc.right - ctxt->width) / 2;
+                dy = (rc.bottom - ctxt->height) / 2;
+                stretched_w = ctxt->width;
+                stretched_h = ctxt->height;
+            }
+            else
+            {
+                auto scaleX = float(wwidth) / ctxt->width;
+                auto scaleY = float(wheight) / ctxt->height;
+                if (scaleX < scaleY)
+                {
+                    stretched_w = LONG(ctxt->width * scaleX + 0.5f);
+                    stretched_h = LONG(ctxt->height * scaleX + 0.5f);
+                    dx = rc.left;
+                    dy = (rc.bottom - stretched_h) / 2;
+                }
+                else
+                {
+                    stretched_w = LONG(ctxt->width * scaleY + 0.5f);
+                    stretched_h = LONG(ctxt->height * scaleY + 0.5f);
+                    dx = (rc.right - stretched_w) / 2;
+                    dy = rc.top;
+                }
+            }
+            StretchBlt(
+                hdc,
+                dx,
+                dy,
+                stretched_w,
+                stretched_h,
+                memDC,
+                0,
+                0,
+                ctxt->width,
+                ctxt->height,
+                SRCCOPY
+                );
+            SelectObject(memDC, oldBmp);
+            DeleteDC(memDC);
+        }
+        else
+        {
+            DrawText(
+                hdc,
+                L"SVG unavailable",
+                -1,
+                &rc,
+                DT_CENTER | DT_VCENTER | DT_SINGLELINE
+                );
+        }
+        EndPaint(hwnd, &ps);
+        return 0;
+
+    case WM_ERASEBKGND:
+        return 1;  // handled in WM_PAINT
+
+    case WM_DESTROY:
+        if (ctxt)
+        {
+            if (ctxt->bitmap)
+            {
+                DeleteObject(ctxt->bitmap);
+            }
+            free(ctxt);
+        }
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WINAPI ListGetDetectString(char* detect_str, int maxlen)
+{
+    const char* ds = "EXT=\"SVG\" | EXT=\"SVGZ\"";
+    strncpy(detect_str, ds, maxlen - 1);
+    detect_str[maxlen - 1] = '\0';
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+HBITMAP WINAPI ListGetPreviewBitmapW(
+    PWSTR fname,
+    int width,
+    int height,
+    char*,
+    int
+    )
+{
+    LONG dummy_w, dummy_h;
+    return bitmap_from_svg(fname, width, height, dummy_w, dummy_h);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+HWND WINAPI ListLoadW(HWND parent, PWSTR fname, int)
+{
+    auto* ctxt = static_cast<SvgCtxt*>(malloc(sizeof(SvgCtxt)));
+    if (!ctxt)
+    {
+        return nullptr;
+    }
+
+    if (wnd_class == 0)
+    {
+        WNDCLASS wc;
+        ZeroMemory(&wc, sizeof(wc));
+        wc.lpfnWndProc    = wnd_proc;
+        wc.hInstance      = hinst();
+        wc.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+        wc.hbrBackground  = HBRUSH(GetStockObject(WHITE_BRUSH));
+        wc.lpszClassName  = L"svg_wlx";
+        wc.style          = CS_HREDRAW | CS_VREDRAW;
+        wnd_class = RegisterClass(&wc);
+        if (wnd_class == 0)
+        {
+            return nullptr;
+        }
+    }
+
+    RECT rc;
+    GetClientRect(parent, &rc);
+    auto width = rc.right - rc.left;
+    auto height = rc.bottom - rc.top;
+    auto hwnd = CreateWindow(
+        MAKEINTRESOURCE(wnd_class),
+        L"",
+        WS_CHILD | WS_VISIBLE,
+        0,
+        0,
+        width,
+        height,
+        parent,
+        nullptr,
+        hinst(),
+        nullptr
+        );
+    if (hwnd)
+    {
+        SetWindowLongPtr(
+            hwnd,
+            GWLP_USERDATA,
+            reinterpret_cast<LONG_PTR>(ctxt)
+            );
+        ctxt->bitmap = bitmap_from_svg(
+            fname,
+            width,
+            height,
+            ctxt->width,
+            ctxt->height
+            );
+    }
+    return hwnd;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WINAPI ListCloseWindow(HWND list_wnd)
+{
+    DestroyWindow(list_wnd);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int WINAPI ListLoadNextW(HWND, HWND svg_wnd, PWSTR fname, int)
+{
+    auto* ctxt = get_ctxt(svg_wnd);
+    if (!ctxt)
+    {
+        return LISTPLUGIN_ERROR;
+    }
+
+    if (ctxt->bitmap)
+    {
+        DeleteObject(ctxt->bitmap);
+        ctxt->bitmap = nullptr;
+    }
+
+    RECT rc;
+    GetClientRect(svg_wnd, &rc);
+    ctxt->bitmap = bitmap_from_svg(
+        fname,
+        rc.right - rc.left,
+        rc.bottom - rc.top,
+        ctxt->width,
+        ctxt->height
+        );
+    InvalidateRect(svg_wnd, nullptr, true);
+    return ctxt->bitmap ? LISTPLUGIN_OK : LISTPLUGIN_ERROR;
+}
+
+////////////////////////////////////////////////////////////////////////////////
